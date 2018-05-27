145cfe827340c36337c6eb02ded83ba82b538514
zhanghaofeng <892433342@qq.com>
2018-05-27 10:23:17 +0800

    统计修改

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,7 @@ const Article =require('../controller/article/article');
 const Check = require('../middlewares/check');
 const router = express.Router();
 
-router.get('/list',  Check.uv,Article.list);//列表
+router.get('/list',  Article.list);//列表Check.uv
 router.get('/detail',Article.detail);//详情
 router.get('/search',Article.listSearch);//文章筛选
 router.post('/del',  Check.checkAdmin,Article.del);//删除
